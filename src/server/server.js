7be249f4c0c03d6e8ee277f6684bb6f5df0925a9
Christopher Moffett <christophmoffett@gmail.com>
2018-06-30 07:01:51 -0700

    Refactored server code into its own dir

--- src/server/server.js
+++ src/server/server.js
@@ -1,18 +1,50 @@
 //Install express server
 const express = require('express');
 const path = require('path');
-
 const app = express();
+const { Pool, Client } = require('pg');
+
+console.log('start');
+var rootpath = path.resolve(__dirname, '../../'); //due to file structure, rootpath to dist folder is up several dir....
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/angular-tour-of-heroes'));
+app.use(express.static(path.join(rootpath +'/dist/angular-tour-of-heroes')));
+//app.use(express.static(__dirname + '/dist/angular-tour-of-heroes'));
 
-app.get('/*', function(req,res) {
-    res.sendFile(path.join(__dirname+'/dist/angular-tour-of-heroes/index.html'));
-});
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
+
+///////////////DB STUFF////////////////////////////
+// console.log(process);
+// console.log(process.env.DATABASE_URL);
+
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true
+});
+
+app.get('/db', async (req, res) => {
+  try {
+  	console.log('connecting to db');
+    const client = await pool.connect()
+    const result = await client.query('SELECT * FROM test_table');
+    res.render('pages/db', result);
+    client.release();
+    
+  } catch (err) {
+    console.error(err);
+    res.send("Error " + err);
+  }
+});
+
+app.get('/*', function(req,res) { //wild card last check
+    console.log('connecting to page');
+    res.sendFile(path.join(rootpath+'/dist/angular-tour-of-heroes/index.html'));
+    //res.sendFile(path.join(__dirname+'/dist/angular-tour-of-heroes/index.html'));
+});
+
 
 ////////////////CLIENT SIDE REQUESTS/////////////////////////
 
